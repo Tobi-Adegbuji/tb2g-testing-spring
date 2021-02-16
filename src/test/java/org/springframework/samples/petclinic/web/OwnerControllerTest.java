a231dc94a0ddb5dc1190b254302626126e725a91
Tobi Adegbuji <adegbuji_tobi@yahoo.com>
2021-02-16 16:29:20 -0500

    100% test coverage for OwnerController processFindForm

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +21,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -41,11 +45,60 @@ class OwnerControllerTest {
 
     MockMvc mockMvc;
 
+    List<Owner> owners;
+
+    private final static String OWNERS_URL = "/owners";
+    private final static String FIND_OWNERS_URL = "owners/findOwners";
+    private final static String REDIRECT_OWNER_URL = "redirect:/owners/";
+    private final static String OWNERS_LIST_URL = "owners/ownersList";
+
+
 
     @BeforeEach
     void setUp() {
         //Sets up Mock Mvc Environment for controller
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+        Owner owner = new Owner();
+        owner.setLastName("sandy");
+        owner.setId(1);
+        Owner owner2 = new Owner();
+        owner2.setLastName("sandy");
+
+        owners = new ArrayList<>(List.of(owner, owner2));
+    }
+
+
+    @Test
+    void findByNameNotFound() throws Exception {
+        mockMvc.perform(get(OWNERS_URL)
+        .param("lastName", (String) null)) //configuring request further bt
+        .andExpect(status().isOk())
+        .andExpect(view().name(FIND_OWNERS_URL));
+    }
+
+    @Test
+    void processFindFormOneOwnerFound() throws Exception{
+        //GIVEN
+        given(clinicService.findOwnerByLastName("sandy"))
+                .willReturn(new ArrayList<>(List.of(owners.get(0))));
+
+        mockMvc.perform(get(OWNERS_URL)
+                .param("lastName", "sandy"))
+                .andExpect(status().isFound())
+                .andExpect(view()
+                        .name(REDIRECT_OWNER_URL + owners.get(0).getId()));
+    }
+
+    @Test
+    void processFindFormOwnersFound() throws Exception{
+        given(clinicService.findOwnerByLastName("sandy"))
+                .willReturn(owners);
+
+       mockMvc.perform(get(OWNERS_URL)
+       .param("lastName", "sandy"))
+               .andExpect(status().isOk())
+               .andExpect(view().name(OWNERS_LIST_URL));
+
     }
 
     @DisplayName("Test should return proper view name given Model")
